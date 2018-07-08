349592ef88fab141047a955bd82fd4b277d62e38
Adam Burucs <adam.burucs@gmail.com>
2018-07-08 13:41:02 +0200

    Update Stats

--- tevekenysegi_mutatoszamok.php
+++ tevekenysegi_mutatoszamok.php
@@ -1,17 +1,10 @@
 <?php include_once "layout/header.php"; ?>
 <?php include_once "layout/menu.php"; ?>
-    <h1 class="page-title">Tevékenységi mutatószámok 2018. I. negyedév</h1>
+    <h1 class="page-title">Tevékenységi mutatószámok 2018. II. negyedév</h1>
       <p>
-      ÁKO (átlagos képzési óraszám): 144,83 óra
-      <br>VSM (vizsga sikerességi mutató) : elmélet 66%  forgalom 57,45%
-      <br>KK (átlagos képzési költség, tényleges befizetési költség): 201.629 Ft
+      ÁKO (átlagos képzési óraszám): 139,43 óra
+      <br>VSM (vizsga sikerességi mutató) : elmélet 59,15%  forgalom 63,84%
+      <br>KK (átlagos képzési költség, tényleges befizetési költség): 196.818 Ft
       </p>
-      <?php /*
-      <p>
-      ÁKO (átlagos képzési óraszám): 161,6 %
-      <br>VSM (vizsga sikerességi mutató) : elmélet 68,18% forgalom 72,73%
-      <br>KK (átlagos képzési költség, tényleges befizetési költség): 169.800 Ft
-      </p>
-        */ ?>
   </div>
 <?php include_once "layout/footer.php"; ?>
